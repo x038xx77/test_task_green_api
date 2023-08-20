ea39e6adc1352b0fd2231d12dd11709215613e3a
x038xx77 <x038xx77@gmail.com>
2023-08-20 01:36:44 -0400

    Success M1 and M2 not docker

--- microservice_m1/src/index.ts
+++ microservice_m1/src/index.ts
@@ -9,22 +9,31 @@ const opts: RouteShorthandOptions = {
     response: {
       200: {
         type: 'object',
-        properties: {}
+        properties: {
+          server_test: {
+            type: 'string'
+          }
+        }
       }
     }
   }
 };
 
+
+server.get('/test', opts, async (request, reply) => {
+  return { server_test: 'it worked!' }
+})
+
 const logger = pino({ level: 'info' });
 
 // Создаем подключение к RabbitMQ
 async function createRabbitMQConnection() {
-  const connection = await amqp.connect('amqp://rabbitmq');
+  const connection = await amqp.connect('amqp://localhost:5672');
 
   const channel = await connection.createChannel();
   const queue = 'orders';
 
-  await channel.assertQueue(queue);
+  await channel.assertQueue(queue, { durable: false }); 
 
   channel.consume(queue, (message: amqp.ConsumeMessage | null) => {
     if (message !== null) {
@@ -33,8 +42,7 @@ async function createRabbitMQConnection() {
       // Обработка полученных данных из RabbitMQ
       // В данном примере просто выводим информацию в консоль
       // server.log.info('Received order from RabbitMQ:', orderData);
-      logger.info(`Received order from RabbitMQ:: ${orderData}`);
-
+      logger.info(`Received order from RabbitMQ: ${JSON.stringify(orderData)}`);
       channel.ack(message);
     }
   });
@@ -45,39 +53,49 @@ createRabbitMQConnection().catch((error) => {
   console.error('Error creating RabbitMQ connection:', error);
 });
 
-// Остальной код остается без изменений
+const confirmationQueue = 'confirmations';
 
-server.post('/create-order', opts, async (request, reply) => {
-
+server.post('/create-order', async (request, reply) => {
   try {
     const orderData = request.body;
 
     // Отправка данных заказа в RabbitMQ
-    const connection = await amqp.connect('amqp://rabbitmq');
-
-
-    
+    const connection = await amqp.connect('amqp://localhost:5672');
     const channel = await connection.createChannel();
+    
     const queue = 'orders';
-
+    const confirmationQueue = 'confirmations'; // Новая очередь для подтверждений
+    
     await channel.assertQueue(queue, { durable: false });
+    await channel.assertQueue(confirmationQueue, { durable: false }); // Объявляем новую очередь
+
+    // Отправляем заказ
     channel.sendToQueue(queue, Buffer.from(JSON.stringify(orderData)));
+
+    // Принимаем подтверждение от M2
+    channel.consume(confirmationQueue, (message) => {
+      if (message !== null) {
+        const confirmationMessage = message.content.toString();
+        logger.info(`Received confirmation from M2: ${confirmationMessage}`);
+        channel.ack(message);
+        
+      }
+    });
 
     reply.send({ message: 'Order created and sent for processing' });
   } catch (error) {
-    // server.log.error("Error create order", error);
-    logger.error("Error create order: ",error);
+    logger.error("Error create order:", error);
     reply.status(500).send({ error: 'Internal Server Error' });
   }
 });
 
 const start = async () => {
   try {
-    await server.listen({ port: 3001 });
+    await server.listen({ port: 3001  });
 
     const address = server.server.address();
     const port = typeof address === 'string' ? address : address?.port;
-    console.log(`Server listening on port ${port}`);
+    console.log(`Server listening on port http://localhost:${port}`);
   } catch (error) {
     // server.log.error(error);
     logger.error(error);
--- microservice_m2/src/index.ts
+++ microservice_m2/src/index.ts
@@ -4,19 +4,23 @@ import pino from 'pino';
 const logger = pino({ level: 'info' });
 
 async function processOrders() {
-  // Замените параметры подключения на актуальные
-  let connection = await amqp.connect('amqp://rabbitmq');
-
-  let channel = await connection.createChannel();
-  let queue = 'orders';
-
+  const connection = await amqp.connect('amqp://localhost:5672');
+  const channel = await connection.createChannel();
+  
+  const queue = 'orders';
+  const confirmationQueue = 'confirmations'; // Новая очередь для подтверждений
+  
   await channel.assertQueue(queue, { durable: false });
+  await channel.assertQueue(confirmationQueue, { durable: false }); // Объявляем новую очередь
 
   channel.consume(queue, (message) => {
     if (message !== null) {
       const orderData = JSON.parse(message.content.toString());
       const confirmationNumber = generateConfirmationNumber();
-      logger.info(`Order processed. Confirmation number: ${confirmationNumber}`);
+      
+      // Отправляем подтверждение в M1
+      channel.sendToQueue(confirmationQueue, Buffer.from(confirmationNumber));
+      logger.info(`Order processed. Confirmation number: ${confirmationNumber} order Data M1 ${JSON.stringify(orderData)}`);
       channel.ack(message);
     }
   });
